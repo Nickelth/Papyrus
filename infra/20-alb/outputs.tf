c663d15ebcfad3b7fb179ab072eb81cba762f91b
Nickdell <nickelth.nike@gmail.com>
2025-10-29 15:11:10 +0900

    CI: ALBセキュリティグループIDをoutputsに追加

--- infra/20-alb/outputs.tf
+++ infra/20-alb/outputs.tf
@@ -1,2 +1,11 @@
-output "alb_dns_name" { value = aws_lb.this.dns_name }
-output "tg_arn" { value = aws_lb_target_group.this.arn }
+output "alb_sg_id" {
+  value = aws_security_group.alb.id
+}
+
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "tg_arn" {
+  value = aws_lb_target_group.this.arn
+}
